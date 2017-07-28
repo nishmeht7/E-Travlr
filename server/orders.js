76f15cfc396d0eca4fd095424c8731a9625d9cb8
Nishant Mehta <nishmeht7@gmail.com>
2017-07-28 18:32:03 -0500

    orders backend routes done

--- server/orders.js
+++ server/orders.js
@@ -2,7 +2,7 @@ const db = require('APP/db')
 const Order = db.model('Order')
 
 module.exports = require('express').Router()
-	.get('/',
+	.get('/', //will only be used for admin purposes to see all orders on system
 		(req, res, next) => 
 		Order.findAll()
 		.then(orders => {
@@ -15,3 +15,15 @@ module.exports = require('express').Router()
 		.then(orders => {
 			res.json(orders)
 		}))
+	.post('/',
+		(req, res, next) => 
+		Order.create(req.body)
+		.then(createdOrder => {
+			res.json(createdOrder)
+		})
+		.catch(next))
+	.delete('/',
+		(req,res,next) => 
+		Order.delete(req.body)
+		.then(() => res.json('order deleted complete'))
+		.catch(next))
