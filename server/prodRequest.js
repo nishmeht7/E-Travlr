7ba96464fb27fc6f31287dabff9dd1c2925e64e9
Charles <charles.m.long43@gmail.com>
2017-07-28 19:04:43 -0500

    typo

--- server/prodRequest.js
+++ server/prodRequest.js
@@ -2,8 +2,8 @@ const db = require('APP/db')
 const ProdRequest = db.model('prodRequests')
 
 module.exports = require('express').Router()
-	.get('/', 
-		(req, res, next) => 
+	.get('/',
+		(req, res, next) =>
 			ProdRequest.findAll()
 			.then(prodRequests => {
 				res.json(prodRequests)
@@ -11,12 +11,12 @@ module.exports = require('express').Router()
 			})
 			.catch(next))
 	.post('/',
-		(req, res, next) => 
+		(req, res, next) =>
 			ProdRequest.Create(req.body)
 			.then(singleRequest => res.status(201).json(singleRequest))
 			.catch(next))
-	.delete('/:id', 
-		(req, res, next) => 
-		ProdRequest.destroy({where{id: req.params.id}})
+	.delete('/:id',
+		(req, res, next) =>
+		ProdRequest.destroy({where: {id: req.params.id}})
 		.then(() => res.json('success'))
 		.catch(next))
