d3196eb1e27b758ad54cfb62e1a2b697128b9f2b
Nishant Mehta <nishmeht7@gmail.com>
2017-08-23 19:06:25 -0500

    initial tests done

--- app/components/Initial.test.jsx
+++ app/components/Initial.test.jsx
@@ -14,8 +14,19 @@ describe('<Initial />', () => {
 		root = shallow(<Initial />)
 	)
 
+	it('shows a heading', () => {
+		const titleText = "Order from Anywhere, Deliveries made Everywhere"
+		expect(root.find('h1').text()).to.equal(titleText)
+	})
+
 	it('shows a shopper form', () => 
 		expect(root.find('input[name="prodUrl"]')).to.have.length(1)
 	)
+
+	it('has a submit button', () => {
+		const submit = root.find('button[type="submit"]')
+		expect(submit).to.have.length(1)
+	})
+
 })
 
